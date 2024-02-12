f0b26dfbb86cf0a5f3205cfd1c8944a59164e717
asadahmed01 <asadatnish@gmail.com>
2024-02-12 14:33:30 -0500

    feat: optimized solution

--- arrays_and_hashing/contains_duplicates.py
+++ arrays_and_hashing/contains_duplicates.py
@@ -14,6 +14,7 @@ from typing import List
 # Input: nums = [1,1,1,3,3,4,3,2,4,2]
 # Output: true
 
+#Brute force solution
 def contains_duplicates(nums: List[int]) -> bool:
     #compare each element against each other elment in the array
     n = len(nums)
@@ -23,5 +24,15 @@ def contains_duplicates(nums: List[int]) -> bool:
                 return True
     return False
 # tests
+
+# Optimized solution
+def contains_duplicates(nums: List[int]) -> bool:
+    hashset = set()
+    for num in nums:
+        if num in hashset:
+            return True
+        hashset.add(num)
+    return False
+
 nums = [1,2,3,1]
 print(contains_duplicates(nums))
