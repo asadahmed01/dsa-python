baae2745a1f7393f5c246b7615618bdaede06b69
asadahmed01 <asadatnish@gmail.com>
2024-02-12 14:16:35 -0500

    feat: find duplicates brute force solution

---
+++ arrays_and_hashing/contains_duplicates.py
@@ -0,0 +1,27 @@
+from typing import List
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+def contains_duplicates(nums: List[int]) -> bool:
+    #compare each element against each other elment in the array
+    n = len(nums)
+    for i in range(n):
+        for j in nums[i+1:]:
+            if nums[i] == nums[j]:
+                return True
+    return False
+# tests
+nums = [1,2,3,1]
+print(contains_duplicates(nums))
